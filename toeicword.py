abf5c11914551fd090cc4387ef273ca054a2da6d
qcds66-cloud <qcds66@gmail.com>
2026-08-02 21:33:32 +0800

    Update toeicword.py

--- toeicword.py
+++ toeicword.py
@@ -84,9 +84,9 @@ st.markdown("""
     .stButton>button {
         width: 300%;
         height: 3.8em;
-        font-size: 1.3rem;
+        font-size: 2.5rem;
         font-weight: bold;
-        border-radius: 6px;
+        border-radius: 10px;
         padding: 10px;
     }
 
