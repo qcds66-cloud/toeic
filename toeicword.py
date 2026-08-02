8b60c2de7cd788125a94914c95dec3cac9d240cf
qcds66-cloud <qcds66@gmail.com>
2026-08-02 21:36:35 +0800

    Update toeicword.py

--- toeicword.py
+++ toeicword.py
@@ -83,7 +83,7 @@ st.markdown("""
     /* 獨立設定：按鍵尺寸與樣式 */
     .stButton>button {
         width: 300%;
-        height: 2.0rem;
+        height: 2.4rem;
         font-size: 2.5rem;
         font-weight: bold;
         border-radius: 10px;
